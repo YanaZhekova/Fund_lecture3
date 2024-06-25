a813f204a450023d14c73aaf3b6f295472f6eed0
YanaZhekova <ianagekowa@gmail.com>
2024-06-25 09:12:05 +0300

    Lecture 3 exercises

---
+++ bread_factory.py
@@ -0,0 +1,41 @@
+events = input().split("|")
+energy = 100
+coins = 100
+make_all_events = True
+for i in events:
+    collect = i.split("-")
+    event = collect[0]
+    number = int(collect[1])
+
+    if event == "rest":
+        if energy + number <= 100:
+            energy += number
+            print(f"You gained {number} energy.")
+            print(f"Current energy: {energy}.")
+        else:
+            print(f"You gained {100-energy} energy.")
+            print(f"Current energy: {100}.")
+
+    elif event == "order":
+        if energy - 30 >= 0:
+            coins += number
+            energy -= 30
+            print(f"You earned {number} coins.")
+        else:
+            if energy + 50 <= 100:
+                energy += 50
+                print("You had to rest!")
+                make_all_events = False
+    else:
+        if coins - number >= 0:
+            coins -= number
+            print(f"You bought {event}.")
+        else:
+            print(f"Closed! Cannot afford {event}.")
+            make_all_events = False
+            break
+
+if make_all_events:
+    print("Day completed!")
+    print(f"Coins: {coins}")
+    print(f"Energy: {energy}")
---
+++ hello_france.py
@@ -0,0 +1,45 @@
+items = input().split("|")
+budget = float(input())
+bought_items = []
+train_ticket = 150
+total_price_items = 0.0
+for i in items:
+    collect = i.split("->")
+    item = collect[0]
+    price = float(collect[1])
+
+    if price <= budget:
+        if item == "Clothes":
+            if price <= 50:
+                budget -= price
+                bought_items.append(price)
+                total_price_items += price
+        elif item == "Shoes":
+            if price <= 35:
+                budget -= price
+                bought_items.append(price)
+                total_price_items += price
+        elif item == "Accessories":
+            if price <= 20.50:
+                budget -= price
+                bought_items.append(price)
+                total_price_items += price
+
+result = []
+sum = 0.0
+for i in bought_items:
+    i = i + i * 0.4
+    i = round(i, 2)
+    sum += i
+    result.append(str(i))
+
+budget += sum
+print(" ".join(result))
+profit = sum - total_price_items
+if len(bought_items) > 0:
+    print(f"Profit: {profit:.2f}")
+
+if budget >= train_ticket:
+    print("Hello, France!")
+else:
+    print("Not enough money.")
